24b6fd884509ebf40c3ffed5d65d402ff3dbebbb
Ksruiz22 <98917570+Ksruiz22@users.noreply.github.com>
2022-04-20 12:24:41 -0500

    Create Skeleton Classes.py

---
+++ Skeleton Classes.py
@@ -0,0 +1,70 @@
+
+class RecipeDisplay(Comparator):
+    def __init__(self):
+        pass
+    def printFilter(filter):
+        pass
+    def updateRecipe(Recipe)-> None:
+        pass
+    def changeIngredients(int)-> None:
+        pass
+    def setVisible(boolean)-> None:
+        pass
+   
+class Comparator:
+    def __init__(self):
+        cookBook: CookBook
+        ingredient: Ingredient
+    def filter(cookBook, ingredient):
+        pass
+         
+class Ingredient:
+        def __init__(self):
+            name: str
+            amount: int
+            unit: str
+
+class User(Ingredient):
+    def __init__(self):
+        ingred: list[Ingredient]
+        amount: list[int]
+        unit: list[str]
+    def getingredient(ingred):
+        pass
+    def getAmount(amount):
+        pass
+    def getUnit(unit):
+        pass
+    
+
+class CookBook:
+    def _init_(self):
+        path: str
+        recipes: list[str]
+        currentRecipe: int
+        currentView: str
+        recDisplay: RecipeDisplay
+    def nextRecipe()-> None:
+        pass
+
+class RecipeReader:
+    def __init__(self, cedula:int, name:str, lastname:str, plan:int, telephone:int, paydate:str, cardnumber:str, expdate:str, cod:int):
+        pass
+        def carmod():
+            pass
+        def modcard():
+            pass
+        def modnum():
+            pass
+
+class Recipe:
+    def _init_(self, name:str, lastname:str, telephone:int, salary:float):
+        pass
+    def addclient():
+        pass
+    def removeclient(): 
+        pass
+    def modclient():
+        pass
+    def modnum():
+        pass
